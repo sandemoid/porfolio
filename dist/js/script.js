7ff85f25a7f6fd6caabe862465ae162d6e32c280
sandemoid <infosandemo@gmail.com>
2022-09-30 16:12:25 +0700

    p

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,14 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('hidden');
 });
 
+let header = document.querySelector("header");
+const animasi = document.querySelector("#animasi");
+window.addEventListener("scroll", () => {
+  let pos = window.scrollY > 0;
+  header.classList.toggle("nav-fixed", pos);
+  animasi.classList.toggle("fixed", pos);
+});
+
 // navbar-fixed
 window.onscroll = function() {
     const header = document.querySelector('header');
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
       screens: { 
         '2xl': '1320px',
       },
+      animation: { 
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: { 
+        'goyang': { 
+          '0%, 100%': { transform: 'rotate(3deg)' },
+          '50%': { transform: 'rotate(-3deg)' },
+        },
+      },
     },
   },
   plugins: [],
